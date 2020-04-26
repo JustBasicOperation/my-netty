06c3e30c301ebb9c4a29464f8ed38354b71195c5
zhaolinfeng3 <zhaolinfenggg@163.com>
2020-04-26 19:41:52 +0800

    添加websocket支持

---
+++ netty-websocket/src/main/java/cn/zlfjw/websocket/server/ChatServer.java
@@ -0,0 +1,57 @@
+package cn.zlfjw.websocket.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.ImmediateEventExecutor;
+
+import java.net.InetSocketAddress;
+
+public class ChatServer {
+    private final ChannelGroup channelGroup = new DefaultChannelGroup(ImmediateEventExecutor.INSTANCE);
+    private final EventLoopGroup group = new NioEventLoopGroup();
+    private Channel channel;
+
+    public ChannelFuture start(InetSocketAddress address){
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(group)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(createInitializer(channelGroup));
+        ChannelFuture future = serverBootstrap.bind(address);
+        future.syncUninterruptibly();
+         channel = future.channel();
+        return future;
+    }
+
+    protected ChannelInitializer<Channel> createInitializer(ChannelGroup group) {        //3
+        return new ChatServerInitializer(group);
+    }
+
+    public void destroy() {        //4
+        if (channel != null) {
+            channel.close();
+        }
+        channelGroup.close();
+        group.shutdownGracefully();
+    }
+
+    public static void main(String[] args) {
+        final ChatServer chatServer = new ChatServer();
+        ChannelFuture future = chatServer.start(new InetSocketAddress(8080));
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+//                super.run();
+                chatServer.destroy();
+            }
+        });
+        future.channel().closeFuture().syncUninterruptibly();
+    }
+}
